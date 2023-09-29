d594920364cc0ede0a15ebccc3631911d7db9f93
Victor Hong <Victor Hong@DESKTOP-1QBH96H>
2023-09-29 22:55:34 +0900

    초기 커밋.

---
+++ example/example.cpp
@@ -0,0 +1,160 @@
+﻿#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+class Ball {
+public:
+	float x, y;
+	int speed_x, speed_y;
+	int radius;
+
+	void Draw() {
+		DrawCircle(x, y , radius, WHITE);
+	}
+
+	void Update() {
+		x += speed_x;
+		y += speed_y;
+
+
+		if (y + radius >= GetScreenHeight()|| y - radius <= 0 ) {
+			speed_y *= -1;
+		}
+
+		if (x + radius >= GetScreenWidth() || x - radius <= 0) {
+			speed_x *= -1;
+
+		}
+	}
+};
+
+class Paddle {
+
+protected:
+
+	void LimitMovement() {
+		if (y <= 0) {
+			y = 0;
+		}
+		if (y + height >= GetScreenHeight()) {
+			y = GetScreenHeight() - height;
+		}
+	}
+
+
+public:
+	float x, y;
+	float width, height;
+	int speed;
+	void Draw() {
+	DrawRectangle(x, y, width, height, WHITE);
+}
+	void Update() {
+		if (IsKeyDown(KEY_UP))
+		{
+			y = y - speed;
+		}
+		if (IsKeyDown(KEY_DOWN))
+		{
+			y = y + speed;
+		}
+
+		LimitMovement();
+	}
+};
+
+class CpuPaddle: public Paddle{
+public:
+
+	void Update(int ball_y) {
+		if (y + height / 2 > ball_y) {
+
+			y = y - speed;
+		}
+		if (y + height / 2 <= ball_y) {
+
+			y = y + speed;
+		}
+
+		LimitMovement();
+
+	}
+};
+
+Ball ball;
+Paddle player;
+CpuPaddle cpu;
+
+int main() {
+
+	cout << "Starting the game" << endl;
+	const int screen_width = 1200;
+	const int screen_height = 800;
+	InitWindow(screen_width, screen_height, "My Pong Game!");
+	SetTargetFPS(60);
+
+	ball.radius = 20;
+	ball.x = screen_width / 2;
+	ball.y = screen_height / 2;
+	ball.speed_x = 7;
+	ball.speed_y = 7;
+
+	player.width = 25;
+	player.height = 120;
+	player.x = screen_width - player.width - 10;
+	player.y = screen_height / 2 - player.height / 2;
+	player.speed = 6;
+
+	cpu.height = 120;
+	cpu.width = 25;
+	cpu.x = 10;
+	cpu.y = screen_height / 2 - cpu.height / 2;
+	cpu.speed = 6;
+
+	
+
+	while (WindowShouldClose() == false) {
+		BeginDrawing();
+
+		//updating 
+		ball.Update();
+		
+		player.Update();
+		cpu.Update(ball.y);
+
+		//Checking for collisions
+	
+		
+		
+		if (CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x,player.y,player.width, player.height}))
+		{
+			ball.speed_x *= -1;
+
+		}
+
+		
+
+		if (CheckCollisionCircleRec(Vector2{ ball.x, ball.y }, ball.radius, Rectangle{ cpu.x,cpu.y,cpu.width, cpu.height }))
+		{
+			ball.speed_x *= -1;
+
+		}
+		
+		
+
+			//Drawing
+			ClearBackground(BLACK);
+			DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+			ball.Draw();
+			cpu.Draw();
+			player.Draw();
+
+
+			EndDrawing();
+		}
+
+	CloseWindow();
+	return 0;
+
+}
